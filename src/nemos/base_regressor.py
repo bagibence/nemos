2b83da8167cda8ecb798eabd09ec124840a7c46d
Bence Bagi <bagi.bence94@gmail.com>
2025-04-01 16:34:30 +0200

    Document BaseRegressor._handle_tolerances

--- src/nemos/base_regressor.py
+++ src/nemos/base_regressor.py
@@ -300,22 +300,53 @@ class BaseRegressor(Base, abc.ABC):
         return all_solver_args
 
     @staticmethod
-    def _handle_tolerances(solver_kwargs, all_solver_args):
+    def _handle_tolerances(
+        solver_kwargs: dict[str, Any], all_solver_args: list[str]
+    ) -> dict:
+        """
+        Handle the potential discrepancy between how tolerance is passed to the
+        solvers and how they expect it.
+        JAXopt used a single `tol` value, while Optimistix uses `rtol` and `atol`.
+        JAXopt solvers have a default value for the tolerance, while Optimistix always
+        expects `rtol` and `atol` explicitly.
+
+        Parameters
+        ----------
+        solver_kwargs:
+            Dict of parameters about to be passed to the solver.
+        all_solver_args:
+            All arguments the solver's methods expect.
+
+        Returns
+        -------
+        solver_kwargs updated to contain the expected tolerance name and value.
+        """
+        if "atol" in solver_kwargs and "tol" in solver_kwargs:
+            raise ValueError("Cannot set both tol and atol in the solver settings.")
+        if "rtol" in solver_kwargs and "tol" in solver_kwargs:
+            raise ValueError("Cannot set both tol and rtol in the solver settings.")
+
+        atol = DEFAULT_ATOL
+
+        # if we get tol but don't need it, use it for atol instead of the default
         if "tol" in solver_kwargs and "tol" not in all_solver_args:
             atol = solver_kwargs.pop("tol")
-        else:
-            atol = DEFAULT_ATOL
 
+        # if we don't get tol but could take it
         if "tol" not in solver_kwargs and "tol" in all_solver_args:
             if "atol" in solver_kwargs:
                 solver_kwargs["tol"] = solver_kwargs.pop("atol")
 
+        # if we don't get atol but need it, set it to the default value or
+        # tol if that's present
         if "atol" not in solver_kwargs and "atol" in all_solver_args:
             solver_kwargs["atol"] = atol
 
+        # if we don't get rtol but need it, set it to the default value
         if "rtol" not in solver_kwargs and "rtol" in all_solver_args:
             solver_kwargs["rtol"] = DEFAULT_RTOL
 
+        # if we get rtol but don't need it, delete it
         if "rtol" in solver_kwargs and "rtol" not in all_solver_args:
             del solver_kwargs["rtol"]
 
