27de4fe9302934d59b91a433856b308cf7e19598
Bence Bagi <bagi.bence94@gmail.com>
2025-04-14 16:34:13 +0200

    Add JaxoptOptaxLBFGS
    Jaxopt has a similar wrapper to optimistix.OptaxMinimiser.
    Included it for benchmarking

---
+++ src/nemos/solvers/_jaxopt_optax_solvers.py
@@ -0,0 +1,62 @@
+from typing import Optional, Union
+
+import jaxopt
+import optax
+
+from ._optimistix_solvers import DEFAULT_ATOL, DEFAULT_RTOL, DEFAULT_MAX_STEPS
+
+
+class JaxoptOptaxLBFGS(jaxopt.OptaxSolver):
+    """
+    Use Optax solvers with JAXopt's wrapper.
+
+    Useful for comparison with Optimistix's wrapper.
+    """
+
+    def __init__(
+        self,
+        fun,
+        atol: float = DEFAULT_ATOL,
+        rtol: float = DEFAULT_RTOL,
+        max_steps: int = DEFAULT_MAX_STEPS,
+        stepsize: float = None,
+        linesearch: Optional[
+            Union[optax.GradientTransformationExtraArgs, optax.GradientTransformation]
+        ] = optax.scale_by_zoom_linesearch(
+            max_linesearch_steps=20,
+            initial_guess_strategy="one",
+        ),
+        implicit_diff: bool = False,
+        verbose: bool = False,
+        jit: bool = True,
+    ):
+        del rtol
+
+        def _fun(params, *args):
+            return fun(params, args)
+
+        _optax_lbfgs = optax.lbfgs(
+            learning_rate=stepsize,
+            linesearch=linesearch,
+        )
+
+        super().__init__(
+            fun=_fun,
+            opt=_optax_lbfgs,
+            tol=atol,
+            maxiter=max_steps,
+            implicit_diff=implicit_diff,
+            verbose=verbose,
+            jit=jit,
+        )
+
+    def init(self, fn, y, args):
+        del fn
+        return self.init_state(y, *args)
+
+    def terminate(self):
+        pass
+
+    @property
+    def max_steps(self):
+        return self.maxiter
