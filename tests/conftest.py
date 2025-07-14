5a992c5b3a74411cb10a637dc737cfa0337d0e78
Bence Bagi <bagi.bence94@gmail.com>
2025-07-14 19:03:25 +0200

    Remove OptimistixLBFGS until optimistix.LBFGS is released

--- tests/conftest.py
+++ tests/conftest.py
@@ -946,7 +946,6 @@ _solver_registry_per_backend = {
         # "ProximalGradient": nmo.solvers.OptaxOptimistixProximalGradient,
         # "ProximalGradient": nmo.solvers.OptimistixProximalGradient,
         "ProximalGradient": nmo.solvers.DirectProximalGradient,
-        # "LBFGS": nmo.solvers.OptimistixLBFGS, # TODO switch to this after Optimistix releases L-BFGS
         "LBFGS": nmo.solvers.OptaxOptimistixLBFGS,
         "BFGS": nmo.solvers.OptimistixBFGS,
         "NonlinearCG": nmo.solvers.OptimistixNonlinearCG,
