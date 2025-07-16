c559ad54adeb8cdb7fba89f2cb31a86f4c7c39e9
Bence Bagi <bagi.bence94@gmail.com>
2025-07-16 14:00:23 +0200

    Remove todo

--- src/nemos/solvers/_direct_optimistix_prox_grad.py
+++ src/nemos/solvers/_direct_optimistix_prox_grad.py
@@ -1,7 +1,5 @@
 """Implementing ProximalGradient with FISTA as an Optimistix IterativeSolver."""
 
-# TODO Implement without acceleration.
-
 from ._optimistix_solvers import OptimistixAdapter
 
 import operator
